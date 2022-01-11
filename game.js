0d53112e49a3903286e2792e3926e5a95c2d1e70
Emili Kaiman <emilikaiman@gmail.com>
2022-01-11 16:38:14 -0700

    Add game class

---
+++ game.js
@@ -0,0 +1,13 @@
+class Game() {
+  constructor() {
+    //two player instances
+    new Player('one', 'X')
+    new Player('two' 'O')
+    //data for game board
+    this.PlayerOnesTurn = true;
+    // gameboards data for win conditions
+    // game is a draw
+    //reset gameboard to new games
+
+  }
+}
